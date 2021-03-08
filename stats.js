af9cd959b13d69eb59ead450f140dbe39821e7dd
Christer Kaitila <breakdance@mcfunkypants.com>
2021-03-08 10:51:24 -0800

    force stats progress bars to stay within 0..1

--- stats.js
+++ stats.js
@@ -1,9 +1,17 @@
+function statPercent(val) {
+	// forces it to stay within 0..1
+    let percent = val / 100;
+    if (percent>1) percent = 1;
+    if (percent<0) percent = 0;
+	return percent;
+}
+
 var statBar1 = createDisplayObject();
 statBar1.draw = function(){
 	rectFill(StatsX,0,160,240,"black");
 	print("MORALE", StatsX, StatsY - 50, 20, "white", 20, "Helvetica");
 	rect(StatsX,StatsY,128,16,4,"white");
-	rectFill(StatsX,StatsY,128 * (stats.energy/100),16,"white");
+	rectFill(StatsX,StatsY,128 * statPercent(stats.energy),16,"white");
 
 	if(yesHover){
 		if(activeCard.yesStats[0] != 0 ){
@@ -20,7 +28,7 @@ var statBar2 = createDisplayObject();
 statBar2.draw = function(){
 	print("QUALITY", StatsX, StatsY*3 - 50, 20, "white", 20, "Helvetica");
 	rect(StatsX,StatsY*3,128,16,4,"white");
-	rectFill(StatsX,StatsY*3,128 * (stats.quality/100),16,"white");
+	rectFill(StatsX,StatsY*3,128 * statPercent(stats.quality),16,"white");
 
 	if(yesHover){
 		if(activeCard.yesStats[1] != 0 ){
@@ -38,7 +46,7 @@ var statBar3 = createDisplayObject();
 statBar3.draw = function(){
 	print("SCHEDULE", StatsX, StatsY*5 - 50, 20, "white", 20, "Helvetica");
 	rect(StatsX,StatsY*5,128,16,4,"white");
-	rectFill(StatsX,StatsY*5,128 * (stats.time/100),16,"white");
+	rectFill(StatsX,StatsY*5,128 * statPercent(stats.time),16,"white");
 
 	if(yesHover){
 		if(activeCard.yesStats[2] != 0 ){
@@ -56,7 +64,7 @@ var statBar4 = createDisplayObject();
 statBar4.draw = function(){
 	print("BUDGET", StatsX, StatsY*7 - 50, 20, "white", 20, "Helvetica");
 	rect(StatsX,StatsY*7,128,16,4,"white");
-	rectFill(StatsX,StatsY*7,128 * (stats.budget/100),16,"white");
+	rectFill(StatsX,StatsY*7,128 * statPercent(stats.budget),16,"white");
 
 	if(yesHover){
 		if(activeCard.yesStats[3] != 0 ){
