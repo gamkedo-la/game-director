d9faf64ef5cc219eff8013f412016e336093d5b5
Christer Kaitila <breakdance@mcfunkypants.com>
2021-04-28 15:27:46 -0700

    stats bars flash when dangerously low

--- stats.js
+++ stats.js
@@ -1,5 +1,6 @@
 const circleX = 148;
 const circleY = 12;
+const LOW_STATS_FLASHING_THRESHOLD = 0.2; // warn when low percentage
 
 function statPercent(val) {
 	// forces it to stay within 0..1
@@ -20,7 +21,11 @@ statBar1.draw = function(){
 	
     printWithShadow("MORALE", StatsX, StatsY+StatsSpacingY-50, 20, "white", 20, "Helvetica");
 	rect(StatsX,StatsY+StatsSpacingY,128,16,4,"white");
-	rectFill(StatsX,StatsY+StatsSpacingY,128 * statPercent(stats.morale),16,"white");
+    var perc = statPercent(stats.morale);
+    var col = "white";
+    var flashing = Math.cos(performance.now()/100)/2+0.5;
+    if (perc < LOW_STATS_FLASHING_THRESHOLD) col = "rgba(255,255,255,"+flashing+")";
+	rectFill(StatsX,StatsY+StatsSpacingY,128 * perc,16,col);
 
 	if(yesHover){
 		if(activeCard.yesStats[0] != 0 ){
@@ -37,7 +42,11 @@ var statBar2 = createDisplayObject();
 statBar2.draw = function(){
 	printWithShadow("QUALITY", StatsX, StatsY+StatsSpacingY*3 - 50, 20, "white", 20, "Helvetica");
 	rect(StatsX,StatsY+StatsSpacingY*3,128,16,4,"white");
-	rectFill(StatsX,StatsY+StatsSpacingY*3,128 * statPercent(stats.quality),16,"white");
+    var perc = statPercent(stats.quality);
+    var col = "white";
+    var flashing = Math.cos(performance.now()/100)/2+0.5;
+    if (perc < LOW_STATS_FLASHING_THRESHOLD) col = "rgba(255,255,255,"+flashing+")";
+	rectFill(StatsX,StatsY+StatsSpacingY*3,128 * perc,16,col);
 
 	if(yesHover){
 		if(activeCard.yesStats[1] != 0 ){
@@ -55,7 +64,11 @@ var statBar3 = createDisplayObject();
 statBar3.draw = function(){
 	printWithShadow("SCHEDULE", StatsX, StatsY+StatsSpacingY*5 - 50, 20, "white", 20, "Helvetica");
 	rect(StatsX,StatsY+StatsSpacingY*5,128,16,4,"white");
-	rectFill(StatsX,StatsY+StatsSpacingY*5,128 * statPercent(stats.time),16,"white");
+    var perc = statPercent(stats.time);
+    var col = "white";
+    var flashing = Math.cos(performance.now()/100)/2+0.5;
+    if (perc < LOW_STATS_FLASHING_THRESHOLD) col = "rgba(255,255,255,"+flashing+")";
+	rectFill(StatsX,StatsY+StatsSpacingY*5,128 * perc,16,col);
 
 	if(yesHover){
 		if(activeCard.yesStats[2] != 0 ){
@@ -73,7 +86,11 @@ var statBar4 = createDisplayObject();
 statBar4.draw = function(){
 	printWithShadow("BUDGET", StatsX, StatsY+StatsSpacingY*7 - 50, 20, "white", 20, "Helvetica");
 	rect(StatsX,StatsY+StatsSpacingY*7,128,16,4,"white");
-	rectFill(StatsX,StatsY+StatsSpacingY*7,128 * statPercent(stats.budget),16,"white");
+    var perc = statPercent(stats.budget);
+    var col = "white";
+    var flashing = Math.cos(performance.now()/100)/2+0.5;
+    if (perc < LOW_STATS_FLASHING_THRESHOLD) col = "rgba(255,255,255,"+flashing+")";
+	rectFill(StatsX,StatsY+StatsSpacingY*7,128 * perc,16,col);
 
 	if(yesHover){
 		if(activeCard.yesStats[3] != 0 ){
